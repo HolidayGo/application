5e8f146fd06bb212934fb0b717f72d9b116fbd53
Connor <31197923+ConnorSage@users.noreply.github.com>
2017-09-25 12:50:46 +1300

    unit test for UseLocation

--- app/src/androidTest/java/holidaygotest/splashtest/UseLocationTest.java
+++ app/src/androidTest/java/holidaygotest/splashtest/UseLocationTest.java
@@ -3,13 +3,10 @@ package holidaygotest.splashtest;
 import android.location.Location;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class UseLocationTest
