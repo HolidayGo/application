53db07141807537ccdb1bad3f714e7568f2ccdda
AreeMartyn <fvg6708@autuni.ac.nz>
2017-09-26 15:24:58 +1300

    Calendar

--- app/src/main/java/holidaygotest/splashtest/OptionCalendar.java
+++ app/src/main/java/holidaygotest/splashtest/OptionCalendar.java
@@ -1,5 +1,6 @@
 package holidaygotest.splashtest;
 
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
@@ -7,9 +8,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
-public class OptionCalendar extends AppCompatActivity
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+public class OptionCalendar extends Activity
 {
+    CalendarView calender;
+    TextView textview1;
+    TextView textview2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -17,7 +31,24 @@ public class OptionCalendar extends AppCompatActivity
         super.onCreate(savedInstanceState);
         Log.d("app","Entered activity: OptionCalendar");
         setContentView(R.layout.activity_optioncalendar);
+        calender = (CalendarView)findViewById(R.id.viewDates);
+        textview1= (TextView)findViewById(R.id.textView1);
+        textview2 = (TextView)findViewById(R.id.textView2);
+
+            calender.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+                @Override
+                public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                    // TODO Auto-generated method stub
+
+                    textview1.setText("Start Date is : " + dayOfMonth + " / " + (month + 1) + " / " + year);
+                }
+            });
+
     }
+
+
+
 
     public void confirm(View view)
     {
@@ -25,6 +56,8 @@ public class OptionCalendar extends AppCompatActivity
         Intent intent = new Intent(this, AppOptions.class);
         Log.d("app","Confirm button pressed");
         startActivity(intent);
+
+
     }
 
     public void back(View view)
@@ -45,6 +78,8 @@ public class OptionCalendar extends AppCompatActivity
                 .show();
 
     }
+
+
 
     @Override
     public void onBackPressed()
